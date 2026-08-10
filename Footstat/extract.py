a6e70b8764805ffb775e60d2344f17a58f2d75ba
Aleks Dimov <aleks.dimov00@gmail.com>
2026-08-10 13:21:30 +0100

    added token and setup the api requests

--- Footstat/extract.py
+++ Footstat/extract.py
@@ -1,8 +1,13 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN=os.getenv("API_KEY")
 
 uri = 'https://api.football-data.org/v4/matches'
-headers = { 'X-Auth-Token': 'UR_TOKEN' }
+headers = { 'X-Auth-Token': TOKEN }
 
 response = requests.get(uri, headers=headers)
 for match in response.json()['matches']:
