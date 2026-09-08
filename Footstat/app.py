37be637d2d1a9cd1632e6a814f0296d32d699674
Aleks Dimov <aleks.dimov00@gmail.com>
2026-09-08 19:57:07 +0100

    added league filter functionality

--- Footstat/app.py
+++ Footstat/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from supabase import create_client, Client
 import pandas as pd
+import altair as alt
 
 @st.cache_resource
 def init_connection() -> Client:
@@ -13,18 +14,18 @@ supabase = init_connection()
 st.title("Footstat")
 options_goals=["1+","2+","3+","4+"]
 filter_button = st.selectbox("Goals", options=options_goals, index=2)
-options_leagues=["Championship England, League 1, League 2, National League"]
 
+options_leagues=["Championship England", "League 1", "League 2", "National League"]
 filter_league_button = st.selectbox("Leagues", options=options_leagues)
 
-def filter_options_table(goals):
+def filter_options_table(goals,league):
     if filter_button == goals:
         goals_selection = int(goals.replace("+" , ""))
         or_filter = f"hometeamscorefull.gte.{goals_selection},awayteamscorefull.gte.{goals_selection}"
         
         response = supabase.table("match").select(
             "League, home_team:team!hometeamid_fk(name), hometeamscorehalf, hometeamscorefull, away_team:team!awayteamid_fk(name), awayteamscorehalf, awayteamscorefull, date"
-        ).or_(or_filter).order("date",desc=True).execute()
+        ).or_(or_filter).eq('League',league).order("date",desc=True).execute()
 
         df = pd.json_normalize(response.data)
 
@@ -34,21 +35,23 @@ def filter_options_table(goals):
 
         return df
 
+# def max_goals_df
 
-
-
-
-result_table = filter_options_table(filter_button)
-league_options = filter_league_button(filter_league_button)
-
-
-
+result_table = filter_options_table(filter_button,filter_league_button)
 
 st.dataframe(result_table,
              use_container_width=False, 
              hide_index=True,)
 
-st.dataframe(league_options,
-             use_container_width=False,
-             hide_index=True)
-
+chart = (
+    alt.Chart(result_table)
+    .mark_bar()
+    .encode(
+        alt.X("Nucleotide:O"),
+        alt.Y("Similarities"),
+        alt.Color("Nucleotide:O"),
+        alt.Tooltip(["Nucleotide", "Similarities"]),
+    )
+    .interactive()
+)
+st.altair_chart(chart)
